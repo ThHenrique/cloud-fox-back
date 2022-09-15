402238504e190fea78f7a3b5861a32cf4b44137e
thhenrique <henriquethiago298@gmail.com>
2022-09-15 01:01:20 -0300

    testando action com docker-compose e vitest

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,4 +1,8 @@
+import 'dotenv/config'
+
 import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 export const AppDataSource = new DataSource({
   "type": "postgres",
